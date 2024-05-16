14ff94c3eeb2a5fa00e02bc7094aa7b3a002a980
damin <34189426+damil27@users.noreply.github.com>
2024-05-16 20:16:27 +0100

    complete book collection crud operation

--- app.js
+++ app.js
@@ -27,15 +27,28 @@ app.get("/collection/:id", (req, res) => {
   }
   return res.status(200).send({ msg: "success", data: book });
 });
-//update 
+//update endpoint 
+app.put("/book_update/:id", (req, res) => {
+    const paramID = parseInt(req.params.id)
+    const book = collection.find(b => b.id === paramID)
+    if (!book) {
+        return res.status(404).send("The resource can not be found!")
+    }
+    const { title, author } = req.body
+    
+    book.title = title,
+    book.author = author
+    return res.status(201).send({msg: "success", data: book})
+})
 
 app.delete("/delete/:id", (req, res) => {
     const paramID = parseInt(req.params.id);
-    const book = collection.find(b => b.id === paramID)
-    if (!book) {
+    const index = collection.findIndex(b => b.id === paramID)
+    if (!index) {
         return res.status(404).send("Book Id can not be found!")
     }
-    
+    collection.splice(index, 1)
+    return res.status(200).send({msg:" Deleted succesfully", index})
 });
 
 app.listen(port, () => {
